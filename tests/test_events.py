a6b7cd43db2e4b1d3a70fe9aad66e5b8ff27a04b
Nick Jackson <nick@dxw.com>
2022-04-03 23:08:38 +0100

    Make response objects richer

---
+++ tests/test_events.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import json
+import pytz
+
+from datetime import datetime
+
+from churchsuite import events
+
+
+def event_factory():
+
+    f = open(Path(__file__).with_name("churchsuite_response.json"))
+    data = json.load(f)
+    return events.Event(data[0], pytz.timezone("Europe/London"))
+
+
+def test_id():
+    event = event_factory()
+
+    assert event.id == 1127446
+
+
+def test_datetime_start():
+    event = event_factory()
+
+    assert event.datetime_start == "2022-02-14 00:00:00"
+
+
+def test_naive_datetime_start():
+    event = event_factory()
+
+    assert event.naive_datetime_start == datetime(2022, 2, 14, 0, 0)
+
+
+def test_localised_datetime_start_has_timezone():
+    event = event_factory()
+
+    assert event.localised_datetime_start.tzinfo != None
+
+
+def test_localised_datetime_start_is_correct():
+    event = event_factory()
+
+    assert event.localised_datetime_start.replace(tzinfo=None) == datetime(
+        2022, 2, 14, 0, 0
+    )
